d374e8cc2ba475f186b4d607a50f4ba6bf80ba9b
Crystal Emo <wendellenli@gmail.com>
2019-05-30 14:17:30 -0700

    latest
    about to revert back to old page

--- js/enter.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-  $('.mybutton').mouseover(function(){
-    $(this).find('img').css({ transform: 'rotate(720deg)' })
-  }).mouseout(function(){
-    $(this).find('img').css({ transform: 'rotate(0deg)' })
-  })
-
-})
